360b71006720574e4c8d9b544b5cb93495c76b1a
Luong Xuan Hoang <hoanqfjfj@gmail.com>
2026-04-05 00:51:41 +0700

    ef

--- src/main/java/com/example/demo/entity/PasswordResetTokenEntity.java
+++ src/main/java/com/example/demo/entity/PasswordResetTokenEntity.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 
@@ -9,7 +10,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-
+@Table(name = "password_reset_tokens")
 @Entity
 public class PasswordResetTokenEntity {
     @Id
